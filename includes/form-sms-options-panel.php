a472556e67649c4071138a892d9850cd34f33750
martinsteel <martinsteel@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-09-29 10:50:59 +0000

    Add i18n calls round hard coded strings
    git-svn-id: https://plugins.svn.wordpress.org/contact-form-7-sms-addon/trunk@445166 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/form-sms-options-panel.php
+++ includes/form-sms-options-panel.php
@@ -9,31 +9,32 @@
 			<td colspan="2">
 				<!-- TODO: make this checkbox work -->
 				<input type="checkbox" id="wpcf7-sms-active" name="wpcf7-sms[active]" value="1" <?php echo ($sms_opt["active"]) ? 'checked="checked"' : '' ?> />
-				<label for="wpcf7-sms-active">Send SMS alerts</label>
+				<label for="wpcf7-sms-active"><?php _e('Send SMS alerts', 'wpcf7_sms') ?></label>
 			</td>
 		</tr>
 		<tr class="wpcf7-sms-row">
 			<td style="width: 50%;">
 				<div class="mail-field">
-					<label for="wpcf7-sms-phone">Mobile number:</label><br />
+					<label for="wpcf7-sms-phone"><?php _e('Mobile number', 'wpcf7_sms') ?>:</label><br />
 					<input type="text" id="wpcf7-sms-phone" name="wpcf7-sms[phone]" class="wide" value="<?php echo esc_attr($sms_opt["phone"]) ?>" />
 				</div>
 				<div class="mail-field">
-					<label for="wpcf7-sms-message">Message body:</label><br />
+					<label for="wpcf7-sms-message"><?php _e('Message body', 'wpcf7_sms') ?>:</label><br />
 					<textarea id="wpcf7-sms-message" name="wpcf7-sms[message]"><?php echo esc_attr($sms_opt["message"]) ?></textarea>
 				</div>
 			</td>
 			<td style="width: 50%;">
 				<div class="mail-field">
-					<label for="wpcf7-sms-username">Mediaburst username:</label><br />
+					<label for="wpcf7-sms-username"><?php _e('Mediaburst username', 'wpcf7_sms') ?>:</label><br />
 					<input type="text" id="wpcf7-sms-username" name="wpcf7-sms[username]" class="wide" value="<?php echo esc_attr($sms_opt["username"]) ?>"/>
 				</div>
 				<div class="mail-field">
-					<label for="wpcf7-sms-password">Mediaburst password:</label><br />
+					<label for="wpcf7-sms-password"><?php _e('Mediaburst password', 'wpcf7_sms') ?>:</label><br />
 					<input type="password" id="wpcf7-sms-password" name="wpcf7-sms[password]" class="wide" value="<?php echo esc_attr($sms_opt["password"]) ?>" />
 				</div>
 				<div class="mail-field">
-					SMS Available: <?php echo $sms_credit ?><br /><a href="https://smsapi.mediaburst.co.uk/" target="_blank">Buy credits</a>
+					<?php _e('SMS Available', 'wpcf7_sms') ?>: <?php echo $sms_credit ?><br />
+					<a href="https://smsapi.mediaburst.co.uk/" target="_blank"><?php _e('Buy credits', 'wpcf7_sms') ?></a>
 				</div>
 			</td>
 		</tr>
