d48f72b6e1c231870ab098a243ef152f61c60bdd
martinsteel <martinsteel@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-17 11:55:17 +0000

    Tag 2.2.0 release
    git-svn-id: https://plugins.svn.wordpress.org/contact-form-7-sms-addon/tags/2.2.0@896070 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- main.php
+++ main.php
@@ -79,20 +79,12 @@ class Clockwork_CF7_Plugin extends Clockwork_Plugin {
   public function send_sms( $form ) {
     $options = array_merge( get_option( 'clockwork_options' ), get_option( 'wpcf7_sms_' . $form->id ) );
     if( isset( $options['api_key'] ) && isset( $options['phone'] ) && $options['phone'] != '' && isset( $options['message'] ) && $options['message'] != '' ) { 
-      
-      // Replace merged Contact Form 7 fields
-      if( defined( 'WPCF7_VERSION' ) && WPCF7_VERSION < 3.1 ) {
-        $regex = '/\[\s*([a-zA-Z_][0-9a-zA-Z:._-]*)\s*\]/';
-      } else {
-        $regex = '/(\[?)\[\s*([a-zA-Z_][0-9a-zA-Z:._-]*)\s*\](\]?)/';
-      }
-      
-      $callback = array( &$form, 'mail_callback' );
-      $message = preg_replace_callback( $regex, $callback, $options['message'] );
-      $phone = preg_replace_callback( $regex, $callback, $options['phone'] );
+
+      $message = $form->replace_mail_tags($options['message']);
+      $phone = $form->replace_mail_tags($options['phone']);
       $phone = explode( ',', $phone );
       $from = $options['from'];
-    
+ 
       try {
         $clockwork = new WordPressClockwork( $options['api_key'] );
         $messages = array();
